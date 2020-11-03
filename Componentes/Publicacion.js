61f0f90de1cf773e1fae8c70172eb654a0d361f3
p09ob <paula8ordonez@gmail.com>
2020-11-02 22:58:25 -0500

    terminado

--- Componentes/Publicacion.js
+++ Componentes/Publicacion.js
@@ -8,11 +8,16 @@ class Publicacion{
         let component = document.createElement('div'); 
         component.className='generalComponent'
         let post = document.createElement('div');
+        post.className = 'post';
         let nombre = document.createElement('div');
+        nombre.className ='nombrePost'
         let comment = document.createElement('input');
+        comment.className = 'inputComment';
         let buttonComment = document.createElement('button');
+        buttonComment.className = 'buttonComment';
         buttonComment.textContent='Comentar';
         let componentComment = document.createElement('div');
+        componentComment.className = 'componentComment';
         let postcomment = document.createElement('div');
         post.innerHTML = (
 
